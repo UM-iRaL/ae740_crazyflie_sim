cf8f6232fd69567d75fb56a13ecb94c4ee364155
Atharva Navsalkar <anavsalkar@gmail.com>
2025-09-24 22:36:35 -0400

    first commit

--- crazyflie-firmware/sitl_make/build/CMakeFiles/version_cf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/atharva/Teaching/AE740_Labs/ae740_crazyflie_sim/crazyflie-firmware/version.c"
-  "CMakeFiles/version_cf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/version_cf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- crazyflie-firmware/sitl_make/build/CMakeFiles/version_cf.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for version_cf.
-# This may be replaced when dependencies are built.
